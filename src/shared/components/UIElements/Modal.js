de1046b37af7e1da73f71316bf48ec1a2fbe44cd
Ivan <ivan.masiuta@gmail.com>
2023-02-09 21:05:04 +0200

    add animation and preparing to add modal window

---
+++ src/shared/components/UIElements/Modal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom"
+
+import './Modal.css'
+
+
+const ModalOverlay = (props) => {
+  
+}
+
+const Modal = (props) => {
+  
+}
+
+export default Modal

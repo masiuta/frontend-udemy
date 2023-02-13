10991456e73ccbe8bc842a06579523b3ad465f1f
Ivan <ivan.masiuta@gmail.com>
2023-02-13 21:27:45 +0200

    add validation for the input field

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,21 @@
-// import React from "react";
+import React from 'react';
 
-// const NewPlace = () => {
-//   return <form className="place-form">
-//     <Input type="text" label="Title" />
-//   </form>
-// }
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../util/validators';
+import './NewPlace.css';
 
-// export default NewPlace
+const NewPlace = () => {
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
+};
+
+export default NewPlace;

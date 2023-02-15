a4935bf6f87180ae6d629d2f8a87f222006bd955
Ivan <ivan.masiuta@gmail.com>
2023-02-15 21:40:01 +0200

    Create my own hook for logic

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './PlaceForm.css';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg',
+    address: '20 W 34th St., New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg',
+    address: '20 W 34th St., New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: 'u2',
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId)
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true,
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true,
+      },
+    },
+    true
+  );
+
+  const placeSubmitHandler = (event) => {
+    event.preventDefault()
+    console.log(formState.inputs);
+    
+  }
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min 5 elements)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="Submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -25,9 +25,9 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: '',
+    value: props.initialValue || '',
     isTouched: false,
-    isValid: false,
+    isValid: props.initialValid || false,
   });
 
 
@@ -35,7 +35,7 @@ const Input = (props) => {
   const { value, isValid} = inputState
 
   useEffect(() => {
-    onInput(id, inputState.value, isValid)
+    onInput(id, value, isValid)
   }, [id, value, isValid, onInput])
 
   const changeHandler = (event) => {

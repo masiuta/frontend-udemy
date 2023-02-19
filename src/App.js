83abc44954f0d4d882be64708bc37a16364d6909
Ivan <ivan.masiuta@gmail.com>
2023-02-19 11:24:51 +0200

    add auth handler

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -21,14 +22,17 @@ const App = () => {
           <Route path="/" exact>
             <Users />
           </Route>
-          <Route path="/:userId/places">
-            <UserPlaces  />
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
